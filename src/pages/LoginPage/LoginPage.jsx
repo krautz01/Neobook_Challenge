180e11af4f8fdbec7cd7806e3e77706ed1cd3e35
Bektur <bekturnurlanov01@gmail.com>
2023-12-19 00:17:37 +0600

    end of LoginPage-layot

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import s from "./LoginPage.module.css";
 import photo from "./../../assets/grocery-cart.jpg";
+import icon from './../../assets/eye-disable.jpg';
 import { useState } from "react";
 
 export default function LoginPage() {
@@ -9,25 +10,25 @@ export default function LoginPage() {
 
   return (
     <div className={s.wrapp}>
-      
+
       <div className={s.block_1}>
         <img src={photo}></img>
       </div>
 
       <div className={s.block_2}>
         <form className={s.container}>
-          <div className={s.login_name}>
-            <label htmlFor="username">Имя пользователя</label>
-            <input type="text" />
+          <div className={s.username}>
+            <input type="text" className={s.input} placeholder="Имя пользователя"/>
           </div>
-          <div className={s.login_password}>
-            <label htmlFor="password">Пароль</label>
-            <input type="password" />
+          <div className={s.password}>
+            <input type="password" className={s.input} placeholder="Пароль"/>
+            <img src={icon}/>
           </div>
-          <button type="submit">Войти</button>
+          <div className={s.forgot_password}>Забыли пароль</div>
+          <button type="submit" className={s.button}>Войти</button>
+          <div className={s.sign_up}>Зарегистрироваться</div>
         </form>
       </div>
-
     </div>
   );
 }
