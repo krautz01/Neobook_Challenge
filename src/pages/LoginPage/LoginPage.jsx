791d3e8e057afd3866b5c7d0f76608d3647f43ce
Bektur <bekturnurlanov01@gmail.com>
2023-12-19 23:44:24 +0600

    work with API

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,31 +1,60 @@
 import React from "react";
 import s from "./LoginPage.module.css";
 import photo from "./../../assets/grocery-cart.jpg";
-import icon from './../../assets/eye-disable.jpg';
-import { useState } from "react";
+import icon from "./../../assets/eye-disable.jpg";
+import { useState, useEffect  } from "react";
+import login from './../../api/api.js'
 
 export default function LoginPage() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  /* useEffect = (() => {
+    axios.get('https://neobook.online/mobi-market/swagger/?format=openapi')
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+  }, []) */
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await login(username, password);
+      // Обработка успешного входа - сохранение токена, перенаправление и т.д.
+      console.log('Успешный вход:', response);
+    } catch (error) {
+      // Обработка ошибок входа
+      console.error('Ошибка входа:', error);
+    }
+  };
 
   return (
     <div className={s.wrapp}>
-
       <div className={s.block_1}>
         <img src={photo}></img>
       </div>
 
       <div className={s.block_2}>
-        <form className={s.container}>
+        <form className={s.container} onSubmit={handleSubmit}>
           <div className={s.username}>
-            <input type="text" className={s.input} placeholder="Имя пользователя"/>
+            <input
+              type="text"
+              className={s.input}
+              placeholder="Имя пользователя"
+            />
           </div>
           <div className={s.password}>
-            <input type="password" className={s.input} placeholder="Пароль"/>
-            <img src={icon}/>
+            <input
+              type="password"
+              className={s.input}
+              placeholder="Пароль"
+            />
+            <img src={icon} />
           </div>
           <div className={s.forgot_password}>Забыли пароль</div>
-          <button type="submit" className={s.button}>Войти</button>
+          <button type="submit" className={s.button}>
+            Войти
+          </button>
           <div className={s.sign_up}>Зарегистрироваться</div>
         </form>
       </div>
