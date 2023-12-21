616b0e9a1df8b164db4bee689e729d78d0d0fa0c
Bektur <bekturnurlanov01@gmail.com>
2023-12-22 00:05:23 +0600

    new structure of project with JWT-tocens

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "./App.css";
-import { Route, Routes, } from "react-router-dom";
-import AuthPage from "./pages/AuthPage/AuthPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <>
+    <Router>
       <Routes>
-        <Route path="/" Component={AuthPage} />
+        <Route path="/" Component={Main} />
+        <Route path="/login" Component={Login} />
+        <Route path="/register" Component={Register} />
       </Routes>
-    </>
+    </Router>
   );
 }
 
